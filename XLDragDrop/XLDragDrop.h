0493fbb4e4c6ea6bfff607a7c03438877a7d7ea1
Richard Wei <xinranmsn@gmail.com>
2011-12-01 16:56:30 +0800

    Initial Commit

---
+++ XLDragDrop/XLDragDrop.h
@@ -0,0 +1,13 @@
+//
+//  XLDragDrop.h
+//  XLDragDrop
+//
+//  Created by Richard Wei on 11-12-1.
+//  Copyright (c) 2011年 Xinranmsn Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLDragDrop : NSObject
+
+@end
