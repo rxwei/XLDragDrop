d91d153f10867fb0fdd581f04cb5e950378a1fed
Alex Gray <alex@mrgray.com>
2012-09-07 17:00:13 -0400

    Added Sample Application that embeds framework and runs test app.

---
+++ XLDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XLDemo
+//
+//  Created by Alex Gray on 9/7/12.
+//  Copyright (c) 2012 Xinranmsn Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
